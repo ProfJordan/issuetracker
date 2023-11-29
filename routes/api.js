1ea004145404ebef417ffca1989ac0a206a43091
ProfJordan <Jordan.Halliday@slcc.edu>
2023-11-29 15:41:14 -0700

    added post logic

--- routes/api.js
+++ routes/api.js
@@ -8,20 +8,34 @@ module.exports = function (app) {
   app.route('/api/issues/:project')
   
     .get(async (req, res) => {
-      let project = req.params.project;
+      let projectName = req.params.project;
+      try {
+        const project = await ProjectModel.findOne({ name: projectName });
+        if(!project) {
+          return res.json({ error: 'project not found' });
+        } else {
+          const issues = await IssueModel.find({ projectId: project._id, ...req.query, });
+          if(!issues) {
+            return res.json({ error: 'no issues found' });
+          }
+          return res.json(issues);
+        }
+      } catch (error) {
+        res.json({ error: 'get error... get failed '});
+      }
       
     })
     
     .post(async (req, res) => {
-      let project = req.params.project;
+      let projectName = req.params.project;
       const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
       if (!issue_title || !issue_text || !created_by) {
         return res.json({ error: 'required field(s) missing' });
       }
       try {
-        let projectModel = await ProjectModel.findOne({ name: project });
+        let projectModel = await ProjectModel.findOne({ name: projectName });
         if (!projectModel) {
-          projectModel = new ProjectModel({ name: project });
+          projectModel = new ProjectModel({ name: projectName });
           projectModel = await projectModel.save();
         }
         const issueModel = new IssueModel({
@@ -38,7 +52,7 @@ module.exports = function (app) {
         const issue = await issueModel.save();
         res.json(issue);
       } catch (error) {
-        res.json({ error: 'post error... post failed ' + error });
+        res.json({ error: 'post error... post failed ', _id: _id});
       }
     })
     
